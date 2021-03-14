7f9e029599c1eb6388a9a38fe15d1925e0e946ff
Guk Kim <gukkim132@gmail.com>
2021-03-14 12:49:42 -0500

    Fix errors caused by incorrect response

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,8 +1,18 @@
-const handleSignin = (req, res, db, bcrypt) => {
+const handleSignin = (req, res, db, database, bcrypt) => {
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json("incorrect form submission");
   }
+
+  // let found = false;
+  // database.users.forEach((user) => {
+  //   if (req.body.email === user.email && req.body.password === user.password) {
+  //     found = true;
+  //     console.log(user);
+  //     return res.json(user);
+  //   }
+  // });
+  // !found ? res.status(400).json("Sign-In Error") : null;
 
   db.select("email", "hash")
     .from("login")
@@ -10,13 +20,40 @@ const handleSignin = (req, res, db, bcrypt) => {
     .then((data) => {
       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
       if (isValid) {
+        console.log(req.body.email);
         db.select("*")
-          .from("users")
+          .from("favorites")
           .where("email", "=", req.body.email)
           .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json("Signning Error"));
+            console.log(user);
+            console.log(!user.length);
+            if (!user.length) {
+              console.log("adsf");
+              res.json({
+                id: "",
+                name: "",
+                email: email,
+                joined: "",
+                favorites: [],
+              });
+            } else {
+              res.json({
+                id: "",
+                name: "",
+                email: email,
+                joined: "",
+                favorites: user,
+              });
+            }
+          });
+        // db.select("*")
+        //   .from("users")
+        //   .where("email", "=", req.body.email)
+        //   .then((user) => {
+        //     console.log(user);
+        //     res.json(user[0]);
+        //   })
+        //   .catch((err) => res.status(400).json("Signning Error"));
       } else {
         res.status(400).json("Wrong Credentials");
       }
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const knex = require("knex");
+const knexPostgis = require("knex-postgis");
 const bcrypt = require("bcrypt-nodejs");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
+
+const pry = require("pry");
 
 const db = knex({
   client: "pg",
@@ -16,6 +19,7 @@ const db = knex({
   },
 });
 
+const st = knexPostgis(db);
 // console.log(db.select("*").from("users"));
 
 const app = express();
@@ -48,40 +52,98 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", (req, res) =>
-  register.handleRegister(req, res, db, bcrypt)
+  register.handleRegister(req, res, db, database, bcrypt)
 );
 
-app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
+app.post("/signin", (req, res) =>
+  signin.handleSignin(req, res, db, database, bcrypt)
+);
 
 app.post("/favorites", (req, res) => {
   const { placeId, coordinates, email } = req.body;
-  let found = false;
-  const data = {
-    placeId: placeId,
-    coordinates: coordinates,
-  };
-  database.users.forEach((user) => {
-    user.email === email
-      ? (user.favorites.push(data), (found = true), res.json(user))
-      : null;
-  });
-  !found ? res.status(400).json("Favorite Error") : null;
+
+  // let found = false;
+  // const data = {
+  //   placeId: placeId,
+  //   coordinates: coordinates,
+  // };
+  // database.users.forEach((user) => {
+  //   user.email === email
+  //     ? (user.favorites.push(data), (found = true), res.json(user))
+  //     : null;
+  // });
+  // !found ? res.status(400).json("Favorite Error") : null;
+  console.log(coordinates, placeId, email);
+
+  db.insert({
+    place_id: placeId,
+    email: email,
+    lat: coordinates.lat,
+    lng: coordinates.lng,
+  })
+    .into("favorites")
+    .then(() => {
+      db.select("*")
+        .from("favorites")
+        .where("email", "=", req.body.email)
+        .then((fav) => {
+          res.json(fav);
+          console.log("fav: ", fav);
+        });
+      // db("favorites").then((favs) => {
+      //   res.json(favs);
+      // });
+    });
+
+  // db.insert({
+  //   placeId: placeId,
+  //   email: email,
+  //   lat: coordinates.lat,
+  //   lng: coordinates.lng,
+  // })
+  //   .into("favorites")
+  //   .then((fav) => {
+  //     db("favorites").then((favs) => {
+  //       res.json(favs);
+  //     });
+  //   });
 });
 
 app.post("/delete", (req, res) => {
   const { placeId, email } = req.body;
-  let targetUser;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.email === email) {
-      user.favorites.forEach((item, ind) => {
-        placeId === item.placeId
-          ? (user.favorites.splice(ind, 1), res.json(user), (found = true))
-          : null;
-      });
-    }
-  });
-  !found ? res.status(400).json("Delete Error") : null;
+  // let targetUser;
+  // let found = false;
+  // database.users.forEach((user) => {
+  //   if (user.email === email) {
+  //     user.favorites.forEach((item, ind) => {
+  //       placeId === item.placeId
+  //         ? (user.favorites.splice(ind, 1), res.json(user), (found = true))
+  //         : null;
+  //     });
+  //   }
+  // });
+  // !found ? res.status(400).json("Delete Error") : null;
+
+  db("favorites")
+    .where({
+      place_id: placeId,
+      email: email,
+    })
+    .delete()
+    .then((fav) => {
+      db.select("*")
+        .from("favorites")
+        .where("email", "=", req.body.email)
+        .then((favs) => {
+          res.json(favs);
+        });
+    })
+    // .then(() => {
+    //   res.status(200).json({ status: "delted" });
+    // })
+    .catch((e) => {
+      res.status(400).json("invalid data");
+    });
 });
 
 app.listen(3030, () => {
